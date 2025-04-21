df2f4d11ad4095b38d598d07be2bede2ef1f3711
pug9483 <2019136060@koreatech.ac.kr>
2025-04-22 00:25:20 +0900

    feat : 팔로우 기능을 구현한다.
    - 자기 자신이면 에러 반환 (Domain)
    - 팔로우 유저 카운트 변경 (Domain)
    - 팔로잉 유저 카운트 변경 (Domain)
    
    ‘팔로우 여부 확인하기’
    1. 비즈니스 로직의 여부에 따라 도메인 객체로 갈 것인지 레포지토리 계층으로 갈 것인지 판단한다.
    2. 현재 요구사항이 없으므로 일단 레포지토리 계층으로 위임한다.
    
    ‘자기 자신이면 에러 반환’
    1. 객체끼리의 비교를 통해서 할 수 있으므로 도메인 계층에서 해결한다.
    
    ‘팔로우 유저 카운트 변경, 팔로잉 유저 카운트 변경, 팔로우 여부 저장 및 유저 변경 사항 저장’
    1. 하나의 트랜잭션으로 처리해야 한다.
    2. 0이하로 떨어지면 안되기 때문에 비즈니스 로직을 포함해서 만들 수 있다.

--- src/main/java/com/sample/user/domain/User.java
+++ src/main/java/com/sample/user/domain/User.java
@@ -3,10 +3,43 @@ package com.sample.user.domain;
 public class User {
 	private final Long id;
 	private final UserInfo info;
+	private final UserRelationCounter followingCounter;
+	private final UserRelationCounter followerCounter;
 
 	public User(Long id, UserInfo info) {
 		this.id = id;
 		this.info = info;
+		followingCounter = new UserRelationCounter();
+		followerCounter = new UserRelationCounter();
+	}
+
+	public void follow(User targetUser) {
+		if (this.equals(targetUser)) {
+			throw new IllegalArgumentException();
+		}
+
+		followingCounter.increase();
+		targetUser.increaseFollowerCount();
+	}
+
+	public void unfollow(User targetUser) {
+		if (this.equals(targetUser)) {
+			throw new IllegalArgumentException();
+		}
+
+		followingCounter.decrease();
+		targetUser.decreaseFollowerCount();
+	}
+
+	/**
+	 * 디미터 법칙을 만족하기 위해 private 메서드로 추출한다.
+	 */
+	private void increaseFollowerCount() {
+		followerCounter.increase();
+	}
+
+	private void decreaseFollowerCount() {
+		followerCounter.decrease();
 	}
 
 	@Override
---
+++ src/main/java/com/sample/user/domain/UserRelationCounter.java
@@ -0,0 +1,18 @@
+package com.sample.user.domain;
+
+public class UserRelationCounter {
+	private int count;
+
+	public UserRelationCounter() {
+		this.count = 0;
+	}
+
+	public void increase() {
+		this.count++;
+	}
+
+	public void decrease() {
+		if(count <= 0) return;
+		this.count--;
+	}
+}
